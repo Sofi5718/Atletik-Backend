cad0e417fae2188c33346cf38db55e643c91c278
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-06-20 15:35:27 +0200

    Deltager DTO

---
+++ src/main/java/exam/atletik/dto/DeltagerDto.java
@@ -0,0 +1,14 @@
+package exam.atletik.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeltagerDto {
+    private String navn;
+    private int alder;
+    private String køn;
+    private String klub;
+}
